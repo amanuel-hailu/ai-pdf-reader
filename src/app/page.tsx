c25ae44953a58fa179c128348155352b562634e4
amanuel-hailu <amanuel.hailu@email.sitel.org>
2023-09-15 13:52:41 -0400

    added and properly configured clerk

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,42 @@
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import { UserButton, auth } from "@clerk/nextjs";
+import { LogIn } from "lucide-react";
+import Link from "next/link";
 
-export default function Home() {
-  return <Button>Click me</Button>;
+export default async function Home() {
+  const { userId } = await auth();
+  const isAuth = !!userId;
+
+  return (
+    <div className="w-screen min-h-screen bg-gradient-to-r from-rose-100 to-teal-100">
+      <div className="absolute -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
+        <div className="flex flex-col items-center text-center">
+          <div className="flex items-center ">
+            <h1 className="mr-3 text-5xl font-semibold">Chat With any PDF</h1>
+            <UserButton afterSignOutUrl="/" />
+          </div>
+          <div className="flex mt-2 ">
+            {isAuth && <Button>Go to Chats</Button>}
+          </div>
+          <p className="max-w-xl mt-1 text-lg text-slate-600">
+            Join millions of students, researchers, and professionals to
+            instantly answer questions and understand research with AI
+          </p>
+
+          <div className="w-full mt-4">
+            {isAuth ? (
+              <h1>fileupload</h1>
+            ) : (
+              <Link href="/sign-in">
+                <Button>
+                  Login to get Started!
+                  <LogIn className="w-4 h-4 ml-2" />
+                </Button>
+              </Link>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
