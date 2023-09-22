23c1138c4db08d328ce11610e17d3cd2b8a98f5d
amanuel-hailu <amanuel.hailu@email.sitel.org>
2023-09-22 15:51:54 -0400

    finished debugging s3 issues

--- src/lib/s3.ts
+++ src/lib/s3.ts
@@ -1,75 +1,52 @@
 import AWS from "aws-sdk";
 
-const { S3_ACCESS_KEY_ID, S3_SECRET_ACCESS_KEY, S3_BUCKET_NAME, S3_REGION } =
-  process.env;
-
-// log AWS config
-console.error("S3_ACCESS_KEY_ID:", S3_ACCESS_KEY_ID);
-console.error("S3_SECRET_ACCESS_KEY:", S3_SECRET_ACCESS_KEY);
-console.error("S3_BUCKET_NAME:", S3_BUCKET_NAME);
-console.error("S3_REGION:", S3_REGION);
-const NEXT_PUBLIC_S3_BUCKET_NAME = process.env.NEXT_PUBLIC_S3_BUCKET_NAME;
-console.error("NEXT_PUBLIC_S3_BUCKET_NAME:", NEXT_PUBLIC_S3_BUCKET_NAME);
-
 export async function uploadToS3(file: File) {
-  if (
-    !S3_ACCESS_KEY_ID ||
-    !S3_SECRET_ACCESS_KEY ||
-    !S3_BUCKET_NAME ||
-    !S3_REGION
-  ) {
-    console.error("Error: Missing AWS configuration environment variables");
-    throw new Error("AWS configuration error");
-  }
-
   try {
     AWS.config.update({
-      accessKeyId: S3_ACCESS_KEY_ID,
-      secretAccessKey: S3_SECRET_ACCESS_KEY,
+      accessKeyId: process.env.NEXT_PUBLIC_S3_ACCESS_KEY_ID,
+      secretAccessKey: process.env.NEXT_PUBLIC_S3_SECRET_ACCESS_KEY,
     });
-
     const s3 = new AWS.S3({
       params: {
-        Bucket: S3_BUCKET_NAME,
+        Bucket: process.env.NEXT_PUBLIC_S3_BUCKET_NAME,
       },
-      region: S3_REGION,
+      region: process.env.NEXT_PUBLIC_S3_REGION,
     });
 
     const file_key =
       "uploads/" + Date.now().toString() + file.name.replace(/ /g, "-");
 
     const params = {
-      Bucket: S3_BUCKET_NAME,
+      Bucket: process.env.NEXT_PUBLIC_S3_BUCKET_NAME!,
       Key: file_key,
       Body: file,
     };
 
-    await s3
+    const upload = s3
       .putObject(params)
       .on("httpUploadProgress", (evt) => {
         console.log(
-          "Uploading to S3...",
+          "uploading to s3...",
           parseInt(((evt.loaded * 100) / evt.total).toString()) + "%"
         );
       })
       .promise();
 
-    console.log("Successfully uploaded to S3!");
+    await upload.then((data) => {
+      console.log("successfully uploaded to S3!", data);
+    });
 
-    return {
+    return Promise.resolve({
       file_key,
       file_name: file.name,
-    };
+    });
   } catch (error) {
-    console.error("Error uploading to S3:", error);
-    throw new Error("Failed to upload to S3");
+    // Give a clear error message to the user
+    console.error("Error uploading to S3", error);
+    throw error;
   }
 }
 
 export function getS3URL(file_key: string) {
-  if (!S3_BUCKET_NAME || !S3_REGION) {
-    console.error("Error: Missing AWS configuration for generating S3 URL");
-    throw new Error("AWS configuration error");
-  }
-  return `https://${S3_BUCKET_NAME}.s3.${S3_REGION}.amazonaws.com/${file_key}`;
+  return `https://${process.env.NEXT_PUBLIC_S3_BUCKET_NAME}.s3.${process.env.NEXT_PUBLIC_S3_REGION}.amazonaws.com/${file_key}`;
 }
