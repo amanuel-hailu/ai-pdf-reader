eac56209c6a2a40075df935b4623f01f7b5f362f
amanuel-hailu <amanuel.hailu@email.sitel.org>
2023-09-22 16:36:05 -0400

    fixing s3 issue

--- src/app/api/create-chat/route.ts
+++ src/app/api/create-chat/route.ts
@@ -1,7 +1,7 @@
 import { db } from "@/lib/db";
 import { chats } from "@/lib/db/schema";
 import { loadS3IntoPinecone } from "@/lib/pinecone";
-import { getS3URL } from "@/lib/s3";
+import { getS3Url } from "@/lib/s3";
 import { auth } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
 
@@ -14,16 +14,14 @@ export async function POST(req: Request, res: Response) {
   try {
     const body = await req.json();
     const { file_key, file_name } = body;
-    console.log("file_key", file_key);
-    console.log("file_name", file_name);
-
+    console.log(file_key, file_name);
     await loadS3IntoPinecone(file_key);
     const chat_id = await db
       .insert(chats)
       .values({
         fileKey: file_key,
         pdfName: file_name,
-        pdfUrl: getS3URL(file_key),
+        pdfUrl: getS3Url(file_key),
         userId,
       })
       .returning({
@@ -34,9 +32,7 @@ export async function POST(req: Request, res: Response) {
       {
         chat_id: chat_id[0].insertedId,
       },
-      {
-        status: 200,
-      }
+      { status: 200 }
     );
   } catch (error) {
     console.error(error);
