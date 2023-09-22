f5b5ddd92c7d60db4b12fc7e70cdcae84056673b
amanuel-hailu <amanuel.hailu@email.sitel.org>
2023-09-22 15:17:14 -0400

    added error handling and logging for production s3 issues

--- src/app/api/create-chat/route.ts
+++ src/app/api/create-chat/route.ts
@@ -7,23 +7,44 @@ import { NextResponse } from "next/server";
 
 // /api/create-chat
 export async function POST(req: Request, res: Response) {
-  const { userId } = await auth();
-  if (!userId) {
-    return NextResponse.json({ error: "unauthorized" }, { status: 401 });
+  let userId;
+  try {
+    const authResult = await auth();
+    userId = authResult.userId;
+    if (!userId) {
+      return NextResponse.json({ error: "unauthorized" }, { status: 401 });
+    }
+  } catch (error) {
+    console.error("Authentication error:", error);
+    return NextResponse.json(
+      { error: "authentication error" },
+      { status: 500 }
+    );
   }
+
   try {
     const body = await req.json();
     const { file_key, file_name } = body;
-    console.log("file_key", file_key);
-    console.log("file_name", file_name);
-
+    console.log("file_key:", file_key);
+    console.log("file_name:", file_name);
     await loadS3IntoPinecone(file_key);
+
+    let s3Url;
+    try {
+      s3Url = getS3URL(file_key);
+      // console log info for debugging
+      console.log("s3Url:", s3Url);
+    } catch (s3Error) {
+      console.error("Error getting S3 URL:", s3Error);
+      throw s3Error; // re-throw to be caught by the outer catch block
+    }
+
     const chat_id = await db
       .insert(chats)
       .values({
         fileKey: file_key,
         pdfName: file_name,
-        pdfUrl: getS3URL(file_key),
+        pdfUrl: s3Url,
         userId,
       })
       .returning({
@@ -39,7 +60,7 @@ export async function POST(req: Request, res: Response) {
       }
     );
   } catch (error) {
-    console.error(error);
+    console.error("Error processing request:", error);
     return NextResponse.json(
       { error: "internal server error" },
       { status: 500 }
--- src/lib/s3.ts
+++ src/lib/s3.ts
@@ -1,52 +1,67 @@
 import AWS from "aws-sdk";
 
+const { S3_ACCESS_KEY_ID, S3_SECRET_ACCESS_KEY, S3_BUCKET_NAME, S3_REGION } =
+  process.env;
+
 export async function uploadToS3(file: File) {
+  if (
+    !S3_ACCESS_KEY_ID ||
+    !S3_SECRET_ACCESS_KEY ||
+    !S3_BUCKET_NAME ||
+    !S3_REGION
+  ) {
+    console.error("Error: Missing AWS configuration environment variables");
+    throw new Error("AWS configuration error");
+  }
+
   try {
     AWS.config.update({
-      accessKeyId: process.env.NEXT_PUBLIC_S3_ACCESS_KEY_ID,
-      secretAccessKey: process.env.NEXT_PUBLIC_S3_SECRET_ACCESS_KEY,
+      accessKeyId: S3_ACCESS_KEY_ID,
+      secretAccessKey: S3_SECRET_ACCESS_KEY,
     });
+
     const s3 = new AWS.S3({
       params: {
-        Bucket: process.env.NEXT_PUBLIC_S3_BUCKET_NAME,
+        Bucket: S3_BUCKET_NAME,
       },
-      region: process.env.NEXT_PUBLIC_S3_REGION,
+      region: S3_REGION,
     });
 
     const file_key =
       "uploads/" + Date.now().toString() + file.name.replace(/ /g, "-");
 
     const params = {
-      Bucket: process.env.NEXT_PUBLIC_S3_BUCKET_NAME!,
+      Bucket: S3_BUCKET_NAME,
       Key: file_key,
       Body: file,
     };
 
-    const upload = s3
+    await s3
       .putObject(params)
       .on("httpUploadProgress", (evt) => {
         console.log(
-          "uploading to s3...",
+          "Uploading to S3...",
           parseInt(((evt.loaded * 100) / evt.total).toString()) + "%"
         );
       })
       .promise();
 
-    await upload.then((data) => {
-      console.log("successfully uploaded to S3!", data);
-    });
+    console.log("Successfully uploaded to S3!");
 
-    return Promise.resolve({
+    return {
       file_key,
       file_name: file.name,
-    });
+    };
   } catch (error) {
-    // Give a clear error message to the user
-    console.error("Error uploading to S3", error);
-    throw error;
+    console.error("Error uploading to S3:", error);
+    throw new Error("Failed to upload to S3");
   }
 }
 
 export function getS3URL(file_key: string) {
-  return `https://${process.env.NEXT_PUBLIC_S3_BUCKET_NAME}.s3.${process.env.NEXT_PUBLIC_S3_REGION}.amazonaws.com/${file_key}`;
+  if (!S3_BUCKET_NAME || !S3_REGION) {
+    console.error("Error: Missing AWS configuration for generating S3 URL");
+    throw new Error("AWS configuration error");
+  }
+  return `https://${S3_BUCKET_NAME}.s3.${S3_REGION}.amazonaws.com/${file_key}`;
 }
